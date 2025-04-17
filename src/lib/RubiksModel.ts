ab20f867c883b77a3b9dcbeb2ac72de41cd12345
JeanJano <jeansauvage6@gmail.com>
2025-04-17 20:35:11 +0200

    add all cube rotation

--- src/lib/RubiksModel.ts
+++ src/lib/RubiksModel.ts
@@ -5,15 +5,31 @@ export default class RubiksModel {
 
     private _cube: THREE.Group;
     private _cubies: THREE.Object3D[];
+    private _axis: THREE.Vector3;
 
     constructor(model: THREE.Group) {
         this._cube = model;
         this._cubies = model.children;
+        this._axis = new THREE.Vector3(0, 0, 0);
         console.log("hello", this._cube)
     }
 
-    public front() {
+    public front(clockwise = true) {
         console.log("front");
+
+        const layerZ = 0.3;
+        const cubies = this._cubies.filter(c => Math.abs(c.position.z - layerZ) < 0.1);
+        this._axis.set(0, 0, 1);
+        this._rotateLayer(cubies, clockwise);
+    }
+
+    public back(clockwise = true) {
+        console.log("back");
+
+        const layerZ = -0.3;
+        const cubies = this._cubies.filter(c => Math.abs(c.position.z - layerZ) < 0.1);
+        this._axis.set(0, 0, 1);
+        this._rotateLayer(cubies, clockwise);
     }
 
     public up(clockwise = true) {
@@ -21,11 +37,38 @@ export default class RubiksModel {
 
         const layerY = 0.3;
         const cubies = this._cubies.filter(c => Math.abs(c.position.y - layerY) < 0.1);
-
-        this._rotateLayer(cubies, new THREE.Vector3(0, 1, 0), clockwise);
+        this._axis.set(0, 1, 0);
+        this._rotateLayer(cubies, clockwise);
     }
 
-    private _rotateLayer(cubies: THREE.Object3D[], axis: THREE.Vector3, clockwise: boolean) {
+    public down(clockwise = true) {
+        console.log("down");
+
+        const layerY = -0.3;
+        const cubies = this._cubies.filter(c => Math.abs(c.position.y - layerY) < 0.1);
+        this._axis.set(0, 1, 0);
+        this._rotateLayer(cubies, clockwise);
+    }
+
+    public right(clockwise = true) {
+        console.log("right");
+
+        const layerX = 0.3;
+        const cubies = this._cubies.filter(c => Math.abs(c.position.x - layerX) < 0.1);
+        this._axis.set(1, 0, 0);
+        this._rotateLayer(cubies, clockwise);
+    }
+
+    public left(clockwise = true) {
+        console.log("left");
+
+        const layerX = -0.3;
+        const cubies = this._cubies.filter(c => Math.abs(c.position.x - layerX) < 0.1);
+        this._axis.set(1, 0, 0);
+        this._rotateLayer(cubies, clockwise);
+    }
+
+    private _rotateLayer(cubies: THREE.Object3D[], clockwise: boolean) {
         const group = new THREE.Group();
         this._cube.add(group);
 
@@ -34,9 +77,9 @@ export default class RubiksModel {
         const angle = (clockwise ? -1 : 1) * Math.PI * 0.5;
 
         gsap.to(group.rotation, {
-            x: axis.x * angle,
-            y: axis.y * angle,
-            z: axis.z * angle,
+            x: this._axis.x * angle,
+            y: this._axis.y * angle,
+            z: this._axis.z * angle,
             duration: 0.5,
             onComplete: () => {
                 console.log("complete")
